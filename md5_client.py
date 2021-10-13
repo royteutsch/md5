725cd1bee10bf4e65b57f1b59033d62bd478b04a
Roy Teutsch <teutschroy@gmail.com>
2021-10-13 13:58:00 +0300

    init

---
+++ md5_client.py
@@ -0,0 +1,37 @@
+import multiprocessing
+import threading
+import socket
+import hashlib
+
+my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+my_socket.connect(('127.0.0.1', 1729))
+
+
+x = True
+
+
+def parse_data(inputted_data, stopper):
+    if inputted_data[0] == "L":  # Server sent the start number and number of processes to be made for each core
+        parameters = inputted_data[1:].split()
+        start_number = parameters[0]
+        processes = parameters[1]
+        goal = parameters[2]
+        for core in range(multiprocessing.cpu_count()):
+            t = threading.Thread(target=decrypt(starting_num=(start_number + (core - 1) * processes),
+                                                num_guesses=processes, goal=goal))
+            t.start()
+
+
+def decrypt(starting_num, num_guesses, goal):
+    for i in range(int(num_guesses)):
+        if hashlib.md5(str(int(starting_num) + i - 1).zfill(10).encode()).hexdigest() == goal:
+            my_socket.send(
+                (("S" + str(int(starting_num) + i - 1).zfill(10)).encode()))
+            my_socket.close()
+            exit()
+
+
+while x:
+    my_socket.send(("C" + str(multiprocessing.cpu_count())).encode())  # Sends the number of cpu cores
+    data = my_socket.recv(1024).decode()
+    parse_data(data, x)
---
+++ md5_server.py
@@ -0,0 +1,48 @@
+import select
+import socket
+
+""" Socket Parameters """
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(('0.0.0.0', 1729))
+server_socket.listen()
+client_sockets = []
+
+""" Decrypting Parameters """
+NUM_OF_TRIES = 1000  # Number of tries per cores
+START_NUMBER = 5000000000  # Number which we start from
+GOAL = "EC9C0F7EDCC18A98B1F31853B1813301"  # The goal of the hash
+FAILURE = True
+
+while FAILURE:
+    rlist, wlist, xlist = select.select([server_socket] + client_sockets, [], [])
+    if FAILURE:
+        for current_socket in rlist:
+            if current_socket is server_socket:
+                connection, client_address = current_socket.accept()
+                print("New client joined!", client_address)
+                client_sockets.append(connection)
+            else:
+                data = current_socket.recv(1024).decode()
+                # print("data: " + data)
+                if data == "" or data == "EXIT":
+                    print("Connection closed: " + str(client_address))
+                    client_sockets.remove(current_socket)
+                    rlist.remove(current_socket)
+                    current_socket.close()
+                else:
+                    if data[0] == "C":  # Client sent us the number of cores he has
+                        # print("Number of Cores for: " + str(client_address) + " is: " + data[1:])
+                        processes = int(data[1:]) * NUM_OF_TRIES
+                        response = "L"+str(START_NUMBER) + " " + str(processes) + " " + GOAL
+                        current_socket.send(response.encode())
+                        START_NUMBER += int(processes)
+                    elif data[0] == "S":  # Client was successful, stop all the processes
+                        print("Decryption successful, number is: " + data[1:])
+                        FAILURE = False
+        print("Current Number: " + str(START_NUMBER).zfill(10))
+    if not FAILURE:
+        for current_socket in rlist:
+            print("Connection closed: " + str(client_address))
+            client_sockets.remove(current_socket)
+            rlist.remove(current_socket)
+            current_socket.close()
